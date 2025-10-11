da71eec14dfd79c77c484b4b660e4730a232fae3
leoavelar <leonardoavelar@protonmail.com>
2025-10-11 21:30:51 +0200

    ENHANCEMENT: Update colors

--- lib/widgets/nearest_shops_widget.dart
+++ lib/widgets/nearest_shops_widget.dart
@@ -826,7 +826,7 @@ class _NearestShopsWidgetState extends State<NearestShopsWidget> {
         decoration: BoxDecoration(
           gradient: isSelected
               ? const LinearGradient(
-            colors: [Color(0xFF8B4513), Color(0xFFD2691E)], // coffeeBrown to chocolate
+            colors: [Color(0xFF8B4513), Color(0xFF8B4513)], // coffeeBrown to chocolate
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
           )
@@ -1192,7 +1192,7 @@ class _NearestShopsWidgetState extends State<NearestShopsWidget> {
               height: 40,
               decoration: BoxDecoration(
                 gradient: const LinearGradient(
-                  colors: [Color(0xFF8B4513), Color(0xFFD2691E)], // coffeeBrown to chocolate
+                  colors: [Color(0xFF8B4513), Color(0xFF8B4513)], // coffeeBrown to chocolate
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                 ),
